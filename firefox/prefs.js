0cb19da1faee7ca854bb869f57f0470ff7bc93e4
jkz1one <mrjoeykeys@gmail.com>
2025-11-18 00:36:52 -0500

    Add Firefox user preferences to prefs.js

---
+++ firefox/prefs.js
@@ -0,0 +1,6 @@
+user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
+user_pref("browser.shell.checkDefaultBrowser", false);
+user_pref("browser.tabs.inTitlebar", 0);
+user_pref("browser.sessionstore.resume_session_once", false);
+user_pref("browser.startup.homepage", "file:///home/pitv/dashboard/index.html");
+user_pref("browser.startup.page", 1);
